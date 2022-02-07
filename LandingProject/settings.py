27a5cc4dd82a8307167ce5b1d5ca2362c28bc905
major <majorofffpavel@gmail.com>
2022-02-07 19:11:26 +0300

    add new app Price make migrate

--- LandingProject/settings.py
+++ LandingProject/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'crm',
     'slider',
+    'priceAPP',
 ]
 
 MIDDLEWARE = [
--- crm/views.py
+++ crm/views.py
@@ -4,4 +4,4 @@ from slider.models import Slider
 
 def first_page(request):
     slider_list = Slider.objects.all()
-    return render(request, './index.html', {'slider_list':slider_list})
+    return render(request, './index.html', {'slider_list': slider_list})
