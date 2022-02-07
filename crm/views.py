94ce590c871628683af78d486c9854bfe9b30e92
major <majorofffpavel@gmail.com>
2022-02-07 16:02:26 +0300

    change settings crm/models varusel.html

--- crm/views.py
+++ crm/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from slider.models import Slider
 
 
 def first_page(request):
-    return render(request, './index.html')
+    slider_list = Slider.objects.all()
+    return render(request, './index.html', {'slider_list':slider_list})
